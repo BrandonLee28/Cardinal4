ebedfd12a9579137baa3525b0a8fca06132c69b0
BrandonLee28 <76419269+BrandonLee28@users.noreply.github.com>
2022-01-03 01:10:24 -0500

    Add files via upload

--- games.py
+++ games.py
@@ -3,6 +3,7 @@ from random import choice
 import discord
 import asyncio
 from discord.ext import commands
+import requests
 
 bot = commands.Bot(command_prefix='.')
 class games(commands.Cog):
@@ -23,6 +24,15 @@ class games(commands.Cog):
             embed = discord.Embed(title="Coinflip",
                                   description=f"{ctx.author.mention} Flipped coin, we got **Tails**!")
             await ctx.send(embed=embed)
+
+    @commands.command()
+    async def animequote(self, ctx):
+        r = requests.get('https://animechan.vercel.app/api/random')
+        embed=discord.Embed(title=Random Anime Quote, color=0xff00c8)
+        embed.add_field(name="Character:", value=r.json()['character'], inline=True)
+        embed.add_field(name="Quote:", value=r.json()['quote'], inline=False)
+        await ctx.send(embed=embed)
+
 
 
 def setup(bot):
