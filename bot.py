d35d8f3897a42a6e6a1a7dbedcaf569a8e2f0de4
BrandonLee28 <76419269+BrandonLee28@users.noreply.github.com>
2022-01-07 22:06:07 -0500

    Add files via upload

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 from discord.ext import commands
 import os
 from dotenv import load_dotenv
+import json
 
 load_dotenv()
 
@@ -25,6 +26,40 @@ async def on_ready():
     activity = discord.Game(name="uh....", type=3)
     await bot.change_presence(status=discord.Status.idle, activity=activity)
     print("Bot is ready!")
+
+@bot.event
+async def on_message(ctx):
+    if ctx.channel.name == "counting":
+        try:
+            number = float(ctx.content)
+            with open('storage.json','r+') as f:
+                data = json.load(f)
+                if number == data['current']+1:
+                    if data['lastcounter'] == ctx.author.id:
+                        await ctx.reply('you cant say it twice in a row you moron..')
+                        data['current'] = 0
+                        f.seek(0)
+                        json.dump(data,f,indent=4)
+                        f.truncate()
+                        await ctx.add_reaction('❌')
+                        await ctx.reply('you suck a counting you are back to 0 now bahahaha.')
+                    else:
+                        data['current'] = data['current'] + 1
+                        data['lastcounter'] = ctx.author.id
+                        f.seek(0)
+                        json.dump(data,f,indent=4)
+                        f.truncate()
+                        await ctx.add_reaction('✅')
+
+                else:
+                    data['current'] = 0
+                    f.seek(0)
+                    json.dump(data,f,indent=4)
+                    f.truncate()
+                    await ctx.add_reaction('❌')
+                    await ctx.reply('you suck a counting you are back to 0 now bahahaha.')
+        except ValueError:
+            return
 
 bot.load_extension('admin')
 bot.load_extension('upload')
